aea0a445c2e8ad3965cc6dd210180cabb9ea6b40
Ada Meceffeuk <46904388+averroes96@users.noreply.github.com>
2021-12-26 00:43:10 +0100

    Serializers & CRUD Ops

--- api/urls.py
+++ api/urls.py
@@ -3,5 +3,10 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('', views.apiOverview),
+    path('', views.apiOverview, name="api_overview"),
+    path('task-list/', views.taskList, name="task_list"),
+    path('details/<str:pk>/', views.details, name="details"),
+    path('create/', views.create, name="create"),
+    path('update/<str:pk>/', views.update, name="update"),
+    path('delete/<str:pk>/', views.delete, name="delete"),
 ]
